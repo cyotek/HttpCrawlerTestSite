9bbbbf3d71de7a4a0716543cdd1cc078dd25169f
Richard Moss <cyotek@cyotek.com>
2015-05-04 19:15:06 +0100

    Added feedback link

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <div class="container">
   <footer class="footer">
     Copyright &copy; 2015 Cyotek Ltd. All Rights Reserved.
+    <p><a href="mailto:support@cyotek.com?subject=<?php echo str_replace('+', '%20', urlencode(SITE_TITLE . ' Feedback (' . $_SERVER["REQUEST_URI"] . ')')) ?>">Comments or suggestions?</a></p>
   </footer>
 </div>
