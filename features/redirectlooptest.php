e2a93793b75e65e708d8cf6b95ad27ea07553dab
Richard Moss <cyotek@cyotek.com>
2015-05-07 19:51:38 +0100

    More whitespace affecting linux

--- features/redirectlooptest.php
+++ features/redirectlooptest.php
@@ -1,5 +1,4 @@
 <?php require_once __DIR__ . '/prepend.php'; ?>
-
 <?php
 header('HTTP/1.1 303 See Other');
 header('Location: ' . getFullyQualifiedUrl(getFeatureRelativeUrl() . 'redirectlooptest.php'));
