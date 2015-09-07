f68122a2090f46e2e1a45a93c648717da94fc6c8
Richard Moss <richard.moss@cyotek.com>
2015-09-07 18:09:43 +0100

    Extended custom attributes
    Added another custom attribute (same name, different tag, unique URL)
    for testing XPath expressions

--- html/attributes/custom.php
+++ html/attributes/custom.php
@@ -8,7 +8,7 @@
  
 ?>
 
-  <p>Although the most commonly used attributes for linking to other resources are <code>href</code> and <code>src</code>, JavaScript can and does make use of any other number of custom attributes based on the requirements of the script author. The example <code>img</code> tag below includes a standard <code>src</code> attribute, but also includes a custom <code>data-original</code> attribute with an alternative image.</p>
+  <p data-original="/assets/img/background4.png">Although the most commonly used attributes for linking to other resources are <code>href</code> and <code>src</code>, JavaScript can and does make use of any other number of custom attributes based on the requirements of the script author. The example <code>img</code> tag below includes a standard <code>src</code> attribute, but also includes a custom <code>data-original</code> attribute with an alternative image. This paragraph also includes a custom <code>data-original</code> attribute.</p>
 
   <img data-original="/assets/img/background3.png" src="/assets/img/background1.png" alt="Background" style="width:  100%;" />
 
