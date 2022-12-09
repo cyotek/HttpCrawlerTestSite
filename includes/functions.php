f08af9c03fcd5c94f9e226520ec7b5c99cb606ca
Richard Moss <richard.moss@cyotek.com>
2022-12-09 18:51:57 +0000

    FIX: getRequestProtocol wasn't working under Cloudflare

--- includes/functions.php
+++ includes/functions.php
@@ -484,10 +484,10 @@ function getBaseUrl()
 
 function getRequestProtocol()
 {
-  if (isset($_SERVER['REQUEST_SCHEME'])) {
-    $protocol = $_SERVER['REQUEST_SCHEME'] . '://';
-  } elseif (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
     $protocol = 'https://';
+  } elseif (isset($_SERVER['REQUEST_SCHEME'])) {
+    $protocol = $_SERVER['REQUEST_SCHEME'] . '://';
   } else {
     $protocol = 'http://';
   }
